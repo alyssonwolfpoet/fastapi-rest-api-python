ac686bbbb7e56231d057a53ce64b8246b7f77fe7
Alysson <alyssos@hotmail.com>
2026-03-24 00:30:48 -0300

    feat: schema and pydantic

--- auth_routes.py
+++ auth_routes.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario
 from dependencies import pega_sessao
 from main import bcrypt_context
+from schemas import UsuarioSchema
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth",tags=["auth"])
 
@@ -13,14 +15,14 @@ async def home():
     return {"mensagem": "Voce acessou a rota order","autenticado": False}
 
 @auth_router.post("/criar_conta")
-async def Criar_conta(email: str, senha: str,nome:str,session= Depends(pega_sessao)):
-    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+async def Criar_conta(usuario_schema: UsuarioSchema,session:Session= Depends(pega_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email==usuario_schema.email).first()
     if usuario:
         # ja existe um usuario com esse email
         raise HTTPException(status_code=400,detail="e-mail do usuario ja cadastrado")
     else:
-        senha_criptografada = bcrypt_context.hash(senha)
-        novo_usuario = Usuario(nome,email,senha_criptografada)
+        senha_criptografada = bcrypt_context.hash(usuario_schema.senha)
+        novo_usuario = Usuario(usuario_schema.nome,usuario_schema.email, senha_criptografada,usuario_schema.ativo,usuario_schema.admin)
         session.add(novo_usuario)
         session.commit()
-        return {"mensagem": "usuario cadastrado com sucesso"}
+        return {"mensagem": f"usuario cadastrado com sucesso {usuario_schema.email}"}
