56a76067f7146737424799a24639e86353ab3351
Alysson <alyssos@hotmail.com>
2026-03-25 23:52:34 -0300

    feat: router login

--- auth_routes.py
+++ auth_routes.py
@@ -2,10 +2,14 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario
 from dependencies import pega_sessao
 from main import bcrypt_context
-from schemas import UsuarioSchema
+from schemas import UsuarioSchema, LoginSchema
 from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth",tags=["auth"])
+
+def criar_token(id_usuario):
+    token = f"kahfhksahgala{id_usuario}"
+    return token
 
 @auth_router.get("/")
 async def home():
@@ -26,3 +30,20 @@ async def Criar_conta(usuario_schema: UsuarioSchema,session:Session= Depends(peg
         session.add(novo_usuario)
         session.commit()
         return {"mensagem": f"usuario cadastrado com sucesso {usuario_schema.email}"}
+    
+# login -> email e senha -> jwt (json web token) ilauhfwkkhfkwahfwçofwçoehifçowfowifpowi
+
+@auth_router.post("/login")
+async def login(login_schema: LoginSchema ,session: Session = Depends(pega_sessao) ):
+    usuario = session.query(Usuario).filter(Usuario.email==login_schema.email).first()
+    if not usuario:
+        raise HTTPException(status_code=400,detail="Usuario não encontrado")
+    else:
+        acess_token = criar_token(usuario.id)
+        return {
+            "acess_token": acess_token,
+            "token_type": "Bearer"
+        }
+        # JWT Bearer
+
+        #h eader =  {"Access-token: Bearer token"}
