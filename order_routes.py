e55f9ad7d35f78d87897cd3d95baf1b0a8566c31
Alysson <alyssos@hotmail.com>
2026-03-29 00:34:08 -0300

    feat: route limit user autorization

--- order_routes.py
+++ order_routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends , HTTPException
 from sqlalchemy.orm import Session
 from dependencies import pega_sessao, verificar_token
 from schemas import PedidoSchema
-from models import Pedido
+from models import Pedido, Usuario
 
 order_router = APIRouter(prefix="/pedidos",tags=["pedidos"],dependencies= [Depends(verificar_token)])
 
@@ -18,10 +18,12 @@ async def criar_pedido(pedido_schema: PedidoSchema,session:Session= Depends(pega
     return {"messagem": f"Pedido criado com sucesso. ID do pedido: {novo_pedido.id}"}
 
 @order_router.post("/pedido/cancelar/{id_pedido}")
-async def cancelar_pedido(id_pedido: int,session:Session= Depends(pega_sessao)):
+async def cancelar_pedido(id_pedido: int,session:Session= Depends(pega_sessao),usuario: Usuario = Depends(verificar_token)):
     pedido = session.query(Pedido).filter(Pedido.id==id_pedido).first()
     if not pedido:
         raise HTTPException(status_code=400, detail="Pedido não encontrado")
+    if not usuario.admin and usuario.id != pedido.usuario:
+        raise HTTPException(status_code=400, detail="Você não tem autorização para fazer essa modificação")
     pedido.status = "CANCELADO"
     session.commit()
     return{
